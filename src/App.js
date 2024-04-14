433440aaade1b44adc75cd07a94a3ba29dbd787c
bazhenak <bazhena.kutergina@gmail.com>
2024-04-15 01:29:40 +0300

    button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function handleClick() {
+    document.getElementsByClassName("text").valueOf('');
+    console.log('button clicked')
+
 }
+const App = (props) => {
+  return (
+      <div className={"body"}>
+          <h3>My name is {props.a}</h3>
+          <input className={"text"} placeholder={"текст: " + props.placeHolder}></input>
+          <button className={"button"}
+                  onClick={handleClick}>{props.description}</button>
+
+      </div>
+  );
+};
 
 export default App;
