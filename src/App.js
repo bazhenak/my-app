5b228e0d6b39d7110a3a7db742647305d33689ae
bazhenak <bazhena.kutergina@gmail.com>
2024-04-22 23:28:53 +0300

    data added 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import React from "react";
 
 function handleClick() {
-    const data = document.getElementById("form").value
-    document.getElementById("form").reset;
+    const data = document.getElementById("form").value='';
+    document.getElementById("form").reset='';
     console.log('button clicked')
 
 }
@@ -13,7 +13,7 @@ const App = (props) => {
       <div className={"body"}>
           <h3>My name is {props.a}</h3>
           <input type={"text"} id={"form"} className={"text"} placeholder={"текст: " + props.placeHolder}></input>
-          <button className={"button"}
+          <button type={"reset"} className={"button"}
                   onClick={handleClick}>{props.description}</button>
 
       </div>
